056290bc90d2be064577afd833c4b80ccd6e7deb
arjunjoshua <arjunjoshua@gmail.com>
2023-08-25 12:31:41 +0100

    Updated CORS headers in addPokemon.js.

--- api/addPokemon.js
+++ api/addPokemon.js
@@ -5,6 +5,11 @@ module.exports = async (req, res) => {
     const { trainerID, lobbyID, pokemonName} = req.body;
     const db = await connectDB();
 
+    // Manually set headers for CORS
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
     const team = await Team.findOne({ trainer: trainerID, lobby: lobbyID });
     // if (team.pokemons.length >= 6) {
     //     return res.status(400).json({ message: 'Team is full' });
