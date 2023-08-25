3ec056ea08c6aaa6e942dd336dfbd57f776f1161
arjunjoshua <arjunjoshua@gmail.com>
2023-08-25 13:08:58 +0100

    Handled CORS validation requests.

--- api/addPokemon.js
+++ api/addPokemon.js
@@ -2,13 +2,19 @@ const connectDB = require('../database/db');
 const { Team } = require('../database/models');
 
 module.exports = async (req, res) => {
+     // Manually set headers for CORS
+     res.setHeader('Access-Control-Allow-Origin', '*');
+     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+     
+    if (req.method === 'OPTIONS') {
+        // Handle the preflight request. Respond successfully:
+        res.status(200).end();
+        return;
+    }   
+    
     const { trainerID, lobbyID, pokemonName} = req.body;
     const db = await connectDB();
-
-    // Manually set headers for CORS
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
     const team = await Team.findOne({ trainer: trainerID, lobby: lobbyID });
     // if (team.pokemons.length >= 6) {
