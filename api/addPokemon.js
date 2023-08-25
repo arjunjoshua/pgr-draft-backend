c41d44fbb05f510a9523c36e7003071c19081841
arjunjoshua <arjunjoshua@gmail.com>
2023-08-25 11:52:54 +0100

    Added support for adding pokemon to the database.

---
+++ api/addPokemon.js
@@ -0,0 +1,15 @@
+const connectDB = require('../database/db');
+const { Team } = require('../database/models');
+
+module.exports = async (req, res) => {
+    const { trainerID, lobbyID, pokemonName} = req.body;
+    const db = await connectDB();
+
+    const team = await Team.findOne({ trainer: trainerID, lobby: lobbyID });
+    // if (team.pokemons.length >= 6) {
+    //     return res.status(400).json({ message: 'Team is full' });
+    // }
+    team.pokemons.push(pokemonName);
+    await team.save();
+    return res.status(200).json({ message: 'Pokemon added to team' });
+};
