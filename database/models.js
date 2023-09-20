8d94dd49da31b686f113c7e86145baa0547b6f04
arjunjoshua <arjunjoshua@gmail.com>
2023-09-20 18:16:00 +0100

    Created a new Script to create a lobby table. This will be used to store the scores of the players in the lobby. Updated models to reflect the same. Updated the recordResult.js to update the nw table.

--- database/models.js
+++ database/models.js
@@ -26,9 +26,20 @@ const MatchSchema = new Schema({
   winnerName: String,
 });
 
+const LobbyScoreSchema = new Schema({
+  lobby: { type: Schema.Types.ObjectId, ref: 'Lobby' },
+  trainer: { type: Schema.Types.ObjectId, ref: 'Trainer' },
+  matchesPlayed: Number,
+  matchesWon: Number,
+  matchesLost: Number,
+  matchesTied: Number,
+  points: Number,
+});
+
 const Trainer = mongoose.model('Trainer', TrainerSchema);
 const Lobby = mongoose.model('Lobby', LobbySchema);
 const Team = mongoose.model('Team', TeamSchema);
 const Match = mongoose.model('Match', MatchSchema);
+const LobbyScore = mongoose.model('LobbyScore', LobbyScoreSchema)
 
-module.exports = { Trainer, Lobby, Team, Match };
+module.exports = { Trainer, Lobby, Team, Match, LobbyScore };
---
+++ scripts/createLobbyScore.js
@@ -0,0 +1,34 @@
+const { Trainer, Lobby, LobbyScore } = require('../database/models');
+const mongoose = require('mongoose');
+
+async function populateTrainerScore() {
+    await mongoose.connect(`mongodb+srv://pgrnewuser:NianticSux12@cluster0.s4iajwl.mongodb.net/pgr-draft?retryWrites=true&w=majority`, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    });
+
+    const db = mongoose.connection;
+    db.on('error', console.error.bind(console, 'connection error:'));
+
+    const lobbies = await Lobby.find();
+
+    for (let lobby of lobbies) {
+        const trainers = await Trainer.find({ lobbies: lobby._id });
+        for(let trainer of trainers) {
+            const lobby_data = {
+                lobby: lobby._id,
+                trainer: trainer._id,
+                matchesPlayed: 0,
+                matchesWon: 0,
+                matchesLost: 0,
+                matchesTied: 0,
+                points: 0,
+            };
+            await LobbyScore.create(lobby_data);
+        }
+    }
+    console.log("New trainer scores have been created.");
+    mongoose.disconnect();
+}
+
+populateTrainerScore();
