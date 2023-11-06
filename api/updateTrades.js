31c6aa5c8e86be543d0761f897f33406402f250f
arjunjoshua <arjunjoshua@gmail.com>
2023-11-06 12:36:39 +0000

    Added error handling to updateTrades.js

--- api/updateTrades.js
+++ api/updateTrades.js
@@ -1,59 +1,68 @@
 const connectDB = require('../database/db');
 const { Trainer, Lobby, Team } = require('../database/models');
 const csv = require('csv-parser');
-const mongoose = require('mongoose');
 const multer = require('multer');
 
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage }).single('file');
 
 module.exports = async (req, res) => {
-    await connectDB();
+    try {
+        await connectDB();
 
-    upload(req, res, async (err) => {
-        if (err) {
-            return res.status(400).send({ error: 'Error uploading file.' });
-        }
+        upload(req, res, async (err) => {
+            if (err) {
+                return res.status(400).send({ error: 'Error uploading file: ' + err.message });
+            }
 
-        const results = [];
-        const bufferStream = new require('stream').PassThrough();
-        bufferStream.end(req.file.buffer);
+            // Check if the file is received and exists
+            if (!req.file || !req.file.buffer) {
+                return res.status(400).send({ error: 'File is undefined or buffer is not available.' });
+            }
 
-        bufferStream
-            .pipe(csv())
-            .on('data', (row) => {
-                results.push(row);
-            })
-            .on('end', async () => {
-                try {
-                    for (const row of results) {
-                        const { trainer, lobby, pokemons } = row;
-                    let returnMessage = '';
-                    let existingTrainer = await Trainer.findOne({ name: trainer });
-                    if (!existingTrainer){
-                     returnMessage += `\n${trainer} not found. `;
-                     continue;
+            const results = [];
+            const bufferStream = new require('stream').PassThrough();
+            bufferStream.end(req.file.buffer);
+
+            bufferStream
+                .pipe(csv())
+                .on('data', (row) => {
+                    results.push(row);
+                })
+                .on('end', async () => {
+                    try {
+                        let returnMessage = '';
+                        for (const row of results) {
+                            const { trainer, lobby, pokemons } = row;
+                            let existingTrainer = await Trainer.findOne({ name: trainer });
+                            if (!existingTrainer) {
+                                returnMessage += `\n${trainer} not found. `;
+                                continue;
+                            }
+                            let existingLobby = await Lobby.findOne({ name: lobby });
+                            if (!existingLobby) {
+                                returnMessage += `\n${lobby} not found. `;
+                                continue;
+                            }
+                            let existingTeam = await Team.findOne({ lobby: existingLobby._id, trainer: existingTrainer._id });
+                            if (!existingTeam) {
+                                returnMessage += `\n${existingTrainer.name} in ${existingLobby.name} not found. `;
+                                continue;
+                            }
+                            const pokemonsArray = pokemons ? pokemons.split(',') : [];
+                            existingTeam.pokemons = pokemonsArray;
+                            await existingTeam.save();
+                        }
+                        returnMessage += '\nExcept the above, all teams updated successfully.';
+                        res.status(200).json({ message: returnMessage });
+                    } catch (err) {
+                        console.log(err);
+                        res.status(500).send({ error: 'Error processing CSV data' });
                     }
-                    let existingLobby = await Lobby.findOne({ name: lobby });
-                    if (!existingLobby) {
-                     returnMessage += `\n ${lobby} not found. `;
-                     continue;
-                    }
-                    let existingTeam = await Team.findOne({lobby: existingLobby._id, trainer: existingTrainer._id});
-                    if (!existingTeam) {
-                     returnMessage += `\n ${existingTrainer.name} in ${existingLobby.name} not found. `;
-                     continue;
-                    }
-                    const pokemonsArray = pokemons ? pokemons.split(',') : [];
-                    existingTeam.pokemons = pokemonsArray;
-                    await existingTeam.save();
-                }
-            returnMessage += '\n Except the above, all teams updated successfully.';
-            res.status(200).json({ message: returnMessage });
-        } catch (err) {
-            console.log(err);
-            res.status(400).send({ error: 'Error processing CSV data' });
-        }
-    });
-})
+                });
+        });
+    } catch (dbConnectErr) {
+        console.log(dbConnectErr);
+        res.status(500).send({ error: 'Failed to connect to database' });
+    }
 };
