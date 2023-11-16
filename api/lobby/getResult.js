9f4c1f7813564a3c0a3a94286b9ea2ac11ee44c8
arjunjoshua <arjunjoshua@gmail.com>
2023-11-16 14:02:32 +0000

    Added a new file to return result of a match

---
+++ api/lobby/getResult.js
@@ -0,0 +1,24 @@
+const { Trainer, Team, Lobby, Match } = require('../../database/models');
+const { connectDB } = require('../../database/db');
+
+module.exports = async (req, res) => {
+    //manually set headers for CORS
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+    if(req.method === 'OPTIONS') {
+        return res.status(200).end();
+    }
+
+    await connectDB();
+    const trainer1ID = req.query.trainer1ID;
+    const trainer2ID = req.query.trainer2ID;
+    const lobbyID = req.query.lobbyID;
+
+    const match = await Match.findOne({trainer1: trainer1ID, trainer2: trainer2ID, lobby: lobbyID});
+    if(!match) {
+        return res.status(400).json({message: 'Match not found'});
+    }
+    return res.status(200).json({match});
+}
