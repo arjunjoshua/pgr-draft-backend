7dc2ae353be8db59d3f76762fe6c35b040785e9f
arjunjoshua <arjunjoshua@gmail.com>
2025-01-16 12:14:36 +0000

    Updated the version of the db and lobbies

--- scripts/createLobbyScore.js
+++ scripts/createLobbyScore.js
@@ -2,7 +2,7 @@ const { Trainer, Lobby, LobbyScore } = require('../database/models');
 const mongoose = require('mongoose');
 
 async function populateTrainerScore() {
-    await mongoose.connect(`mongodb+srv://pvp_v25:NianticSux12@cluster0.s4iajwl.mongodb.net/pvp-draft-v25?retryWrites=true&w=majority`, {
+    await mongoose.connect(`mongodb+srv://pvp_v26:NianticSux12@cluster0.s4iajwl.mongodb.net/pvp-draft-v25?retryWrites=true&w=majority`, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
     });
