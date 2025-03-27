91723b7bed45de1da44f621cd3db24acf278a7b5
arjunjoshua <arjunjoshua@gmail.com>
2025-03-27 16:48:26 +0000

    Created a new endpoint to update env variable Added a new API call to restart deployment

--- functions/update_env_variable.js
+++ functions/update_env_variable.js
@@ -18,6 +18,34 @@ const update_env_variable = (version_number) => {
         .then(response => response.json())
         .then(data => console.log(data))
         .catch(err => console.error(err));
+
+    // restart the server
+    const restart_url = `https://api.vercel.com/v13/deployments`;
+    const restart_body = {
+        name: 'production',
+        gitSource: {
+            ref: process.env.GIT_COMMIT_REF,
+            repoId: process.env.GIT_REPO_ID,
+            type: 'github',
+        }
+    }
+
+    const restart_options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${process.env.VERCEL_TOKEN}`
+        },
+        body: JSON.stringify(restart_body)
+    }
+
+    fetch(restart_url, restart_options)
+        .then(response => response.json())
+        .then(data => console.log(data))
+        .catch(err => console.error(err));
+
+
+
 }
 
 
